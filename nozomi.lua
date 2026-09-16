0a25465391fcd11fd96f42b0fc919a02f7f09d72
kyotocantcode <xqtn@proton.me>
2026-09-16 15:13:24 +1000

    Update nozomi.lua

--- nozomi.lua
+++ nozomi.lua
@@ -1,3 +1,2 @@
 -- dont skid my shit pls
-loadstring(game:HttpGet("https://api.junkie-development.de/api/v1/luascripts/public/7a2767051f5a685551d98ba06402da5a3a7d3324938684b211fa971c9cb26f2e/download"))()
-
+game:GetService("Players").LocalPlayer:Kick("this is discontinued, join discord.gg/uselilac for something better!!!!")
